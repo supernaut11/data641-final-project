69ec0ba5325bc19d4e6d84e80d9591b904dfadbf
colleenaulton <colleen.aulton@gmail.com>
2021-05-15 14:02:13 -0400

    Update personality_and_essay.py

--- personality_and_essay.py
+++ personality_and_essay.py
@@ -37,23 +37,24 @@ neu_status = data[['#AUTHID','cNEU']].drop_duplicates()
 
 df = pd.merge(group_status, neu_status, on=["#AUTHID"])
 
-#df.head()
-
-
 essay_data = pd.read_csv('wcpr_essays.csv', encoding='cp1252')
 
-#essay_data.head()
 
 def split_into_lemmas(text): # creates function to lemmatize words
     text = str(text).lower() # converts all words to lowercase
     words = TextBlob(text).words # returns all words
     return [word.lemmatize() for word in words] # lemmatizes every word
 
-
-def lr_rf_classify():
+def lr_rf_classify(train, test, fold_no, essay = False):
+    X_train, X_test, y_train, y_test = train_test_split(df['STATUS'], df['cNEU'])
+    if essay == True:
+        X_test = essay_data['TEXT']
+        y_test = essay_data['cNEU']
+    
     forest = RandomForestClassifier()
     lr_classifier = LogisticRegression()
-    vect = CountVectorizer(analyzer=split_into_lemmas, decode_error='replace', min_df=2, stop_words='english', ngram_range=(1,2), max_features=100000)
+    #vect = CountVectorizer(analyzer=split_into_lemmas, decode_error='replace', min_df=2, stop_words='english', ngram_range=(1,2), max_features=100000)
+    vect = CountVectorizer(stop_words='english', ngram_range=(1,2), max_features=100000)
     
     X_train_dtm = vect.fit_transform(X_train)
     X_test_dtm = vect.transform(X_test)
@@ -63,29 +64,22 @@ def lr_rf_classify():
     predicted_labels = forest.predict(X_test_dtm)
     print('Test set: ', X_test_dtm.shape)
     print('\n')
-    print('Random Forest Accuracy: ', metrics.accuracy_score(y_test, y_pred_class))
+    print('Random Forest Fold',str(fold_no),'Accuracy:',accuracy_score(y_test,predicted_labels))
     for label in ['y', 'n']:
         print(' Precision for label {} = {}'.format(label, metrics.precision_score(predicted_labels, y_test, pos_label=label)))
         print(' Recall for label {} = {}'.format(label, metrics.recall_score(predicted_labels, y_test, pos_label=label)))
+    rf_acc.append(accuracy_score(y_test, predicted_labels))
         
     lr_classifier.fit(X_train_dtm, y_train)
     y_pred_class = lr_classifier.predict(X_test_dtm)
     predicted_labels = lr_classifier.predict(X_test_dtm)
-    print('Logistic Regression Accuracy: ', metrics.accuracy_score(y_test, y_pred_class))
+    print('Logistic Regression Fold',str(fold_no),'Accuracy:',accuracy_score(y_test,predicted_labels))
     for label in ['y', 'n']:
         print(' Precision for label {} = {}'.format(label, metrics.precision_score(predicted_labels, y_test, pos_label=label)))
         print(' Recall for label {} = {}'.format(label, metrics.recall_score(predicted_labels, y_test, pos_label=label)))
+    lr_acc.append(accuracy_score(y_test, predicted_labels))
     print('\n')
 
-print('Testing on personality data')
-X_train, X_test, y_train, y_test = train_test_split(df['STATUS'], df['cNEU'], random_state=42)
-lr_rf_classify()
-
-
-print('Testing on essay data')
-X_test = essay_data['TEXT']
-y_test = essay_data['cNEU']
-lr_rf_classify()
 
 # Testing K Folds
 # Adapted from https://www.analyseup.com/python-machine-learning/stratified-kfold.html
@@ -93,39 +87,34 @@ lr_rf_classify()
 skf = StratifiedKFold(n_splits=5)
 target = df.loc[:,'cNEU']
 
+print('Testing on personality data')
 rf_acc = list()
 lr_acc = list()
-
-def train_model(train, test, fold_no):
-    forest = RandomForestClassifier()
-    lr_classifier = LogisticRegression()
-    
-    X_train, X_test, y_train, y_test = train_test_split(df['STATUS'], df['cNEU'])
-    #X_test = essay_data['TEXT']
-    #y_test = essay_data['cNEU']
-    
-    vect = CountVectorizer(stop_words='english', ngram_range=(1,2), max_features=100000)
-    
-    X_train_dtm = vect.fit_transform(X_train)
-    X_test_dtm = vect.transform(X_test)
-    
-    forest.fit(X_train_dtm,y_train)
-    predictions = forest.predict(X_test_dtm)
-    print('Random Forest Fold',str(fold_no),'Accuracy:',accuracy_score(y_test,predictions))
-    rf_acc.append(accuracy_score(y_test, predictions))
-    
-    lr_classifier.fit(X_train_dtm,y_train)
-    predictions = lr_classifier.predict(X_test_dtm)
-    print('Logistic Regression Fold',str(fold_no),'Accuracy:',accuracy_score(y_test,predictions))
-    lr_acc.append(accuracy_score(y_test, predictions))
     
 fold_no = 1
 for train_index, test_index in skf.split(df, target):
     train = df.loc[train_index,:]
     test = df.loc[test_index,:]
-    train_model(train,test,fold_no)
+    lr_rf_classify(train,test,fold_no)
+    fold_no += 1
+    
+print('\n')
+print('Random Forest Average Accuracy: ',statistics.mean(rf_acc))
+print('Logistic Regression Average Accuracy: ',statistics.mean(lr_acc))
+print('\n')
+    
+print('Testing on essay data')
+rf_acc = list()
+lr_acc = list()
+    
+fold_no = 1
+for train_index, test_index in skf.split(df, target):
+    train = df.loc[train_index,:]
+    test = df.loc[test_index,:]
+    lr_rf_classify(train,test,fold_no,essay=True)
     fold_no += 1
 
 print('\n')
 print('Random Forest Average Accuracy: ',statistics.mean(rf_acc))
 print('Logistic Regression Average Accuracy: ',statistics.mean(lr_acc))
+print('\n')
