5a2754c805c9f6586a4251678a1cb68278f87346
colleenaulton <colleen.aulton@gmail.com>
2021-05-14 20:50:53 -0400

    Update personality_and_essay.py
    Testing K Folds

--- personality_and_essay.py
+++ personality_and_essay.py
@@ -65,3 +65,36 @@ X_test = essay_data['TEXT']
 y_test = essay_data['cNEU']
 lr_rf_classify()
 
+# Testing K Folds
+
+skf = StratifiedKFold(n_splits=5)
+target = df.loc[:,'cNEU']
+
+model = RandomForestClassifier()
+
+fold_acc = list()
+
+def train_model(train, test, fold_no):
+    X_train, X_test, y_train, y_test = train_test_split(df['STATUS'], df['cNEU'])
+    #X_test = essay_data['TEXT']
+    #y_test = essay_data['cNEU']
+    
+    vect = CountVectorizer(stop_words='english', ngram_range=(1,2), max_features=100000)
+    
+    X_train_dtm = vect.fit_transform(X_train)
+    X_test_dtm = vect.transform(X_test)
+    
+    model.fit(X_train_dtm,y_train)
+    predictions = model.predict(X_test_dtm)
+    print('Fold',str(fold_no),'Accuracy:',accuracy_score(y_test,predictions))
+    fold_acc.append(accuracy_score(y_test, predictions))
+    
+fold_no = 1
+for train_index, test_index in skf.split(df, target):
+    train = df.loc[train_index,:]
+    test = df.loc[test_index,:]
+    train_model(train,test,fold_no)
+    fold_no += 1
+
+print('\n')
+print('Average Accuracy: ',statistics.mean(fold_acc))
