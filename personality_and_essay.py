3b47ed6ee63bac73ba14c7f9aa574004add57f1d
colleenaulton <colleen.aulton@gmail.com>
2021-05-14 20:16:35 -0400

    Create personality_and_essay.py
    Testing with essay dataset

---
+++ personality_and_essay.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import numpy as np
+import scipy as sp
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+from textblob import TextBlob, Word
+import nltk
+import re
+
+
+data = pd.read_csv('wcpr_mypersonality.csv', encoding='cp1252')
+
+array_agg = lambda x: ' '.join(x.astype(str))
+
+group_status = data.groupby(['#AUTHID']).agg({'STATUS': array_agg})
+neu_status = data[['#AUTHID','cNEU']].drop_duplicates()
+
+df = pd.merge(group_status, neu_status, on=["#AUTHID"])
+
+#df.head()
+
+
+essay_data = pd.read_csv('wcpr_essays.csv', encoding='cp1252')
+
+#essay_data.head()
+
+
+def lr_rf_classify():
+    forest = RandomForestClassifier()
+    lr_classifier = LogisticRegression()
+    vect = CountVectorizer(stop_words='english', ngram_range=(1,2), max_features=100000)
+    
+    X_train_dtm = vect.fit_transform(X_train)
+    X_test_dtm = vect.transform(X_test)
+    
+    forest.fit(X_train_dtm, y_train)
+    y_pred_class = forest.predict(X_test_dtm)
+    predicted_labels = forest.predict(X_test_dtm)
+    print('Test set: ', X_test_dtm.shape)
+    print('\n')
+    print('Random Forest Accuracy: ', metrics.accuracy_score(y_test, y_pred_class))
+    for label in ['y', 'n']:
+        print(' Precision for label {} = {}'.format(label, metrics.precision_score(predicted_labels, y_test, pos_label=label)))
+        print(' Recall for label {} = {}'.format(label, metrics.recall_score(predicted_labels, y_test, pos_label=label)))
+        
+    lr_classifier.fit(X_train_dtm, y_train)
+    y_pred_class = lr_classifier.predict(X_test_dtm)
+    predicted_labels = lr_classifier.predict(X_test_dtm)
+    print('Logistic Regression Accuracy: ', metrics.accuracy_score(y_test, y_pred_class))
+    for label in ['y', 'n']:
+        print(' Precision for label {} = {}'.format(label, metrics.precision_score(predicted_labels, y_test, pos_label=label)))
+        print(' Recall for label {} = {}'.format(label, metrics.recall_score(predicted_labels, y_test, pos_label=label)))
+    print('\n')
+
+print('Testing on personality data')
+X_train, X_test, y_train, y_test = train_test_split(df['STATUS'], df['cNEU'], random_state=42)
+lr_rf_classify()
+
+
+print('Testing on essay data')
+X_test = essay_data['TEXT']
+y_test = essay_data['cNEU']
+lr_rf_classify()
+
